3a12b17b2dce86e4a6875eadac24a694cefc461e
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-06-16 22:11:20 +0530

    added cors

--- http-servers/exp-mid.js
+++ http-servers/exp-mid.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:56015',
+}));
 
 app.get('/', function (req, res) {
     res.send("Hello World");
@@ -11,15 +15,15 @@ app.get('/', function (req, res) {
 app.post('/add',function(req,res){
     const a = parseInt(req.body.a);
     const b = parseInt(req.body.b);
-    const result = a + b;
-    res.json({ result });
+    const ans = a + b;
+    res.json({ ans });
 });
 
 app.post('/subtract',function(req,res){
     const a = parseInt(req.body.a);
     const b = parseInt(req.body.b);
-    const result = a - b;
-    res.json({ result });
+    const ans = a - b;
+    res.json({ ans });
 });
 
 app.listen(3000);
