0b8cd00f2b14ba1ab3e83972378995fe1fb43ce9
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-06-17 14:29:12 +0530

    added tokens jwt

--- authentication/index.js
+++ authentication/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 const users = [
 ];
 
+app.get("/me", (req, res) => {
+    const token = req.headers.authorization;
+    const user = users.find(user => user.token === token);
+    if (user) {
+        res.send({
+            username: user.username,
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+})
+
 function generateToken() {
     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -21,7 +35,9 @@ app.post("/login",(req,res)=> {
     const {username, password} = req.body;
     if(users.find(user => user.username === username && user.password === password)) {
         const token = generateToken();
+        users.push({username, password, token});
         res.status(200).send({token});
+        console.log(users);
     } else {
         res.status(400).send("Login failed");
     }
@@ -33,8 +49,7 @@ app.post("/signup",(req,res)=>{
         res.status(400).send("Account already exists");
     } else{
         users.push({username, password});
-        const token = generateToken();
-        res.status(200).send({token});
+        res.status(200).send("Signup successful");
     }
 })
 
