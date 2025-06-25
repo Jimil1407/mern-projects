1d22e3be88b3bee0d396a89b4c19333ee28250db
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-06-25 15:23:32 +0530

    exported hooks as separate files

--- custom-hooks/counter-hook/src/App.jsx
+++ custom-hooks/counter-hook/src/App.jsx
@@ -1,38 +1,19 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-
-function useFetch(url) {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(url);
-        const result = await response.json();
-        setData(result);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [url]);
-
-  return { data, loading, error };
-}
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
+
+  const [value, setValue] = useState(1);
   const { data, loading, error } = useFetch(
-    "https://jsonplaceholder.typicode.com/todos/1"
+    "https://jsonplaceholder.typicode.com/todos/" + value
   );
 
   return (
     <div>
+      <button onClick={() => setValue(1)}>1</button>
+      <button onClick={() => setValue(2)}>2</button>
+      <button onClick={() => setValue(3)}>3</button>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       <pre>{JSON.stringify(data, null, 2)}</pre>
