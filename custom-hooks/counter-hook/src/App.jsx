d065a51407bedab587d4e7ef0dfd0d38897e8655
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-06-25 15:01:57 +0530

    added useFetch hook

--- custom-hooks/counter-hook/src/App.jsx
+++ custom-hooks/counter-hook/src/App.jsx
@@ -1,27 +1,43 @@
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function useCounter() {
+function useFetch(url) {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  function increaseCounter(){
-    setCounter(counter+1);
-  }
-  return {
-    counter: counter,
-    increaseCounter: increaseCounter
-  }
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
 }
 
 function App() {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos/1"
+  );
 
-  const {counter, increaseCounter} = useCounter();
   return (
     <div>
-      <button onClick={increaseCounter}> Increase {counter}</button>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
