79d7a7ea414b527fbc96fd6b456c8c1690ea29ac
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2025-07-07 21:24:26 +0530

    tried joins manual imp

--- postgres/index.ts
+++ postgres/index.ts
@@ -1,38 +1,43 @@
 // write a function to create a users table in your database.
-import { Client } from 'pg'
-import express from 'express'
+import { Client } from "pg";
+import express from "express";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 const client = new Client({
-    connectionString: "postgresql://neondb_owner:npg_O56ZQfjWeKBy@ep-ancient-voice-a80enuhl-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
-  })
+  connectionString:
+    "postgresql://neondb_owner:npg_O56ZQfjWeKBy@ep-ancient-voice-a80enuhl-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
 
-app.post('/signup', async (req, res) => {
-    await client.connect()
-    const {username, email, password} = req.body
-    const user = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`
-    const result = await client.query(user, [username, email, password])
-    //console.log(result)
-    res.json({message: "signup successfull", user: result.rows[0]})
-   console.log(result)
-})
+app.post("/signup", async (req, res) => {
+  await client.connect();
+  const { user_id, city, country, street, pincode } = req.body;
+  const user = `INSERT INTO addresses (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5);`;
+  const result = await client.query(user, [
+    user_id,
+    city,
+    country,
+    street,
+    pincode,
+  ]);
+  //console.log(result)
+  res.json({ message: "signup successfull", user: result.rows[0] });
+  console.log(result);
+});
 
-app.get('/users',async(req,res) => {
-    await client.connect()
-    const result = await client.query(`SELECT * FROM users;`)
-    res.json(result.rows)
-    await client.end() 
-})
-
+app.get("/users", async (req, res) => {
+  const id = req.query.id;
+  await client.connect();
+  const result = await client.query(`SELECT * FROM users where id = $1;`, [id]);
+  const address = await client.query(
+    `SELECT * FROM addresses where user_id = $1;`,
+    [id]
+  );
+  res.json({ user: result.rows[0], address: address.rows });
+  await client.end();
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
-
-
-
-
-
-
+  console.log("Server is running on port 3000");
+});
